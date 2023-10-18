30c446d00466c98d35968e15b0ff86fcbf38235f
drewschmidt145 <drewschmidt145@gmail.com>
2023-10-18 13:56:15 -0700

    Add 3 main files, used parcel to format project
    when active, basic files should be in place, core of the projects is now complete

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import { createRoot } from 'react-dom/client';
+
+// import statement to indicate that you need to bundle ./index.scss
+import "./index.scss";
+
+// Main Component (will eventually use all the others)
+const MyFLixApplication = () => {
+  return (
+    <div className="my-flix">
+      <div>Good Morning</div>
+    </div>
+  );
+};
+
+// finds the root of the app
+const container = document.querySelector("#root");
+const root = createRoot(container);
+
+// tells React to redner your app i the root DOM element
+root.render(<myFlixApplication />);
