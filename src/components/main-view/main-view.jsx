4fbe16b6d6642e42f43d4a09aacf4f8466d3093d
drewschmidt145 <drewschmidt145@gmail.com>
2023-10-24 12:53:55 -0700

    fixed imports

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { MovieCard } from "../movie-card/movie-card.jsx";
-import { MovieView } from "../movie-view/movie-view.jsx";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
   const [movie, setMovies] = useState([
